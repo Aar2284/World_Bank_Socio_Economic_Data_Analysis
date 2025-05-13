74ef2c826016d69681e2501e3d6c724e05f1a049
Aar2284 <153257219+Aar2284@users.noreply.github.com>
2025-05-14 01:35:53 +0530

    Update project.py

--- project.py
+++ project.py
@@ -1,7 +1,11 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score, mean_squared_error
 
 data = pd.read_excel("WorldBank.xlsx")
 # print(data.info())
